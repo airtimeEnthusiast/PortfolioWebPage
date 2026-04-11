716b839c591c0587482ef0b7a331d9017499267d
airtimeEnthusiast <thewrightguy101@gmail.com>
2026-04-10 17:16:57 -0700

    Added ShadCN overlay

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,31 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        border: 'var(--border)',
+        ring: 'var(--ring)',
+        input: 'var(--input)',
+        muted: 'var(--muted)',
+        'muted-foreground': 'var(--muted-foreground)',
+        card: 'var(--card)',
+        'card-foreground': 'var(--card-foreground)',
+        popover: 'var(--popover)',
+        'popover-foreground': 'var(--popover-foreground)',
+        primary: 'var(--primary)',
+        'primary-foreground': 'var(--primary-foreground)',
+        secondary: 'var(--secondary)',
+        'secondary-foreground': 'var(--secondary-foreground)',
+        accent: 'var(--accent)',
+        'accent-foreground': 'var(--accent-foreground)',
+        destructive: 'var(--destructive)'
+      },
+      borderRadius: {
+        lg: 'var(--radius)'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
