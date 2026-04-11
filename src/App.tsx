770f458691b173e079ed66195bdf66ceefa7d339
airtimeEnthusiast <thewrightguy101@gmail.com>
2026-04-10 17:20:45 -0700

    chore: move pages into src/ui/pages and update imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Layout from '@/ui/components/Layout'
-import Home from '@/pages/Home'
-import Projects from '@/pages/Projects'
-import Hobbies from '@/pages/Hobbies'
+import Home from '@/ui/pages/Home'
+import Projects from '@/ui/pages/Projects'
+import Hobbies from '@/ui/pages/Hobbies'
 
 export default function App() {
   return (
